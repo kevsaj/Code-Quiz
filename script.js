28793fe78fc4434730f176cab55328bbd169e5d9
Kevin Sajan <kevsaj@gmail.com>
2020-12-06 18:31:07 -0500

    show test question

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
+var myQuestions = [{
+    question: 'test',
+    answers: {
+        a: '1',
+        b: '2',
+        c: '3'
+    },
+    correctAnswer: 'b'
+}, ];
+
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
     function showQuestions(questions, quizContainer) {
-        
+        var output = [];
+        var answers;
+
+        for (var i = 0; i < questions.length; i++) {
+
+            answers = [];
+
+            for (letter in questions[i].answers) {
+
+                answers.push(
+                    '<label>' +
+                    '<input type="radio" name="question' + i + '" value="' + letter + '">' +
+                    letter + ': ' +
+                    questions[i].answers[letter] +
+                    '</label>'
+                );
+            }
+
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>' +
+                '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        quizContainer.innerHTML = output.join('');
     }
+    
 
     function showResults(questions, quizContainer, resultsContainer) {
-    
+
     }
 
-    
+
     showQuestions(questions, quizContainer);
 
-    
     submitButton.onclick = function () {
         showResults(questions, quizContainer, resultsContainer);
     }
 }
 
-var myQuestions = [
-	{
-		question: test,
-		answers: {
-            a: '1',
-			b: '2',
-			c: '3'
-		},
-		correctAnswer: 'b'
-	},
-];
 
 
 previousButton.addEventListener("click");
