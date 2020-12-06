cfea0fbbcdbcfecf94e55c90647dc649e5cb63a1
Kevin Sajan <kevsaj@gmail.com>
2020-12-06 18:09:45 -0500

    buttons and js ideas

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-script.js
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+    function showQuestions(questions, quizContainer) {
+        
+    }
+
+    function showResults(questions, quizContainer, resultsContainer) {
+    
+    }
+
+    
+    showQuestions(questions, quizContainer);
+
+    
+    submitButton.onclick = function () {
+        showResults(questions, quizContainer, resultsContainer);
+    }
+}
+
+var myQuestions = [
+	{
+		question: test,
+		answers: {
+            a: '1',
+			b: '2',
+			c: '3'
+		},
+		correctAnswer: 'b'
+	},
+];
+
+
+previousButton.addEventListener("click");
+nextButton.addEventListener("click");
+submitButton.addEventListener("click");
