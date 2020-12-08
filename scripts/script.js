0d6e055c6087eb9038894fb87cfcc436d0e87369
Kevin Sajan <kevsaj@gmail.com>
2020-12-07 22:43:23 -0500

    slides for questions and submit button

--- scripts/script.js
+++ scripts/script.js
@@ -38,6 +38,26 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 
     function submitButton(questions, quizContainer, resultsContainer) {
 
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+
+        var userAnswer = '';
+        var numCorrect = 0;
+
+        for (var i = 0; i < questions.length; i++) {
+
+            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+            if (userAnswer === questions[i].correctAnswer) {
+                numCorrect++;
+
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            else {
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     }
 
 
@@ -47,6 +67,33 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
         submitButton(questions, quizContainer, resultsContainer);
     }
 }
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+        previousButton.style.display = 'none';
+    } else {
+        previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    } else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
 
 function startTimer() {
     var tobj = document.getElementById("timespent")
@@ -70,8 +117,13 @@ if (window.addEventListener) {
     window.attachEvent("onload", startTimer);
 }
 
+// Pagination
+const previousButton = document.getElementById("previousButton");
+const nextButton = document.getElementById("nextButton");
+let currentSlide = 0;
 
+showSlide(currentSlide);
 
-previousButton.addEventListener("click");
-nextButton.addEventListener("click");
-submitButton.addEventListener("click");
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+submitButton.addEventListener("click", submit);
